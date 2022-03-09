3cc1762beffce2ec7685a02e6c567058d6ae8a76
a.terohid <s.aliterohid@gmail.com>
2022-03-09 13:35:50 +0330

    complete menu components

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const Menu = () => {
+import styles from "./AppStyle.module.css"
+
+const Menu = ({ items }) => {
     return (
-        <div>
-          menu COMPONENTS  
-        </div>
+      <div className='section-center'>
+      {items.map(( menuItem ) => {
+        const { id , title , img , desc , price } = menuItem;
+        return (
+          <article key={ id } className={ styles.menu_item }>
+            <img src={ img } alt={ title } className={ styles.photo } />
+            <div className={ styles.item_info }>
+              <header>
+                <h4>{ title }</h4>
+                <h4 className={ styles.price } >${ price }</h4>
+              </header>
+              <p className={ styles.item_text } >{ desc }</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
     );
 };
 
